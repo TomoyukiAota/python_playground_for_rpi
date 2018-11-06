c0ae6ca3dc0a214bcb650e74289a29542a208d32
Tomoyuki Aota <tomoyukia@gmail.com>
2018-11-07 04:49:30 +0900

    Add led_blink.py

---
+++ sotech_mycode/6-4/led_blink.py
@@ -0,0 +1,15 @@
+#! /user/bin/env python3.6
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.cleanup()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+while True:
+    GPIO.output(4, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(4, GPIO.LOW)
+    time.sleep(1)
